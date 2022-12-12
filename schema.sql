9e73b180014692de5e8e1f73315052eba1a7d3f6
amare1990 <amaremek@gmail.com>
2022-12-12 23:49:46 +0300

    Alter id field of anomals table

--- schema.sql
+++ schema.sql
@@ -1,3 +1,8 @@
 /* Database schema to keep the structure of entire database. */
 
  create table animals (id INT, name varchar(30), date_of_birth date, escape_attempts INT, neutered boolean, weight_kg decimal);
+
+
+/* Altered the id filed of animals table using the following commands */
+/* alter table animals
+alter column id add generated always as identity; */
