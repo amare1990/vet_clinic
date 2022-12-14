21176687e55ec145916f163fb1c562bada975afa
amare1990 <amaremek@gmail.com>
2022-12-14 19:59:54 +0300

    Create species and owners table, and add foreign keys in animals table to reference to the two tables created

--- schema.sql
+++ schema.sql
@@ -11,3 +11,35 @@ alter column id add generated always as identity; */
 
 ALTER TABLE animals ADD COLUMN species VARCHAR(30);
 
+
+
+/* Third milestone-3rd day */
+create table owners (id INT GENERATED ALWAYS AS IDENTITY, full_name VARCHAR(40), age INT);
+
+create table species (id INT GENERATED ALWAYS AS IDENTITY, name VARCHAR(30) PRIMARY KEY(id));
+
+alter table animals
+alter column id add generated always as identity;
+
+ALTER TABLE animals
+DROP COLUMN species;
+
+ALTER TABLE animals
+add column species_id INT;
+
+/* making species_id as foreign key */
+ALTER TABLE animals                                                                         
+ADD CONSTRAINT constraint_fk
+FOREIGN KEY (species_id) 
+REFERENCES species (id) ON DELETE CASCADE; 
+
+ALTER TABLE animals
+add column owner_id INT;
+
+ALTER TABLE animals
+ADD CONSTRAINT constraint_fk_owner
+FOREIGN KEY (owner_id) 
+REFERENCES owners (id) ON DELETE CASCADE;
+
+
+
